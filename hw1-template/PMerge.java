55de91cda2aac464df6431e0c8ecd92f74d58139
Reva <iam.reva.vaidya@gmail.com>
2023-01-29 17:50:54 -0600

    initialized class and bin search func

--- hw1-template/PMerge.java
+++ hw1-template/PMerge.java
@@ -15,4 +15,42 @@ public class PMerge {
         // fixed thread pool
         // edge case: upper bound and lower bound binary search for A B
     }
+
+    public class sorted_arrays implements Runnable{
+        private int[] A;
+        private int[] B;
+        private int[] C;
+
+        public sorted_arrays(int[] A, int[]B, int[] C){
+            // need to create an object for each side so that we can call
+            // fork() on each part of the array
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        @Override
+        public void run(){
+
+        }
+
+        public int binarySearch(int[] arr, int elem) { //modify so that it returns the index of where
+            int low = 0;
+            int high = arr.length - 1;
+            int mid = -1 ;
+            while (low != high) {
+                mid = (low + high) / 2;
+                if (arr[mid] == elem) {
+                    return mid;
+                }
+                if (elem > arr[mid]){
+                    low = mid +1 ;
+                }else if(elem < arr[mid]){
+                    high = mid - 1;
+                }
+            }
+            return mid;
+        }
+    }
 }
+
+
