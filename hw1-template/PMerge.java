1bde40a0413f4091094529109559b05012cf2cff
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-01-29 22:35:40 -0600

    edited parallelMerge

--- hw1-template/PMerge.java
+++ hw1-template/PMerge.java
@@ -14,15 +14,27 @@ public class PMerge {
         // use binary search
         // fixed thread pool
         // edge case: upper bound and lower bound binary search for A B
+
+        // creates an executor with max number of threads numThreads
+        // if send more tasks than numThreads, then they will be blocked until some are free
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        // create the main task, sort_arrays
+        // this is the initial call
+        sort_arrays toSort = new sort_arrays(A, B, C, numThreads);
+        executor.submit(toSort); // submit() can return result of computation, execute() has return type void
+
+        // manual shutdown
+        executor.shutdown();
     }
 
-    public class sorted_arrays implements Runnable{
+    public static class sort_arrays implements Runnable{
         private int[] A;
         private int[] B;
         private int[] C;
         private int tNum; //which number thread
 
-        public sorted_arrays(int[] A, int[]B, int[] C, int tNum){
+        public sort_arrays(int[] A, int[]B, int[] C, int tNum){
             // need to create an object for each side so that we can call
             // fork() on each part of the array
             this.A = A;
