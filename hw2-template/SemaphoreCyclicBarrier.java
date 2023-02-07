a5e6549e3663607b40a5c864fa92b4ca20956534
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-02-07 00:28:17 -0600

    working on Q1

--- hw2-template/SemaphoreCyclicBarrier.java
+++ hw2-template/SemaphoreCyclicBarrier.java
@@ -2,11 +2,19 @@
 // EID 2
 
 
+import java.util.concurrent.Semaphore;
+
 /* Use only semaphores to accomplish the required synchronization */
 public class SemaphoreCyclicBarrier implements CyclicBarrier {
+    // CyclicBarrier is a synchronization aid that allows a set of threads
+    // to wait for all other threads to reach a common point
 
-    private int parties;
+    private int parties; // threads that need to synchronize their execution
     // TODO Add other useful variables
+
+    private boolean barrierActive;
+    private Semaphore flag = new Semaphore(parties);
+    private int activeParties;
 
     public SemaphoreCyclicBarrier(int parties) {
         this.parties = parties;
@@ -24,8 +32,33 @@ public class SemaphoreCyclicBarrier implements CyclicBarrier {
      * indicates the first to arrive and (parties-1) indicates
      * the last to arrive.
      */
-    public int await() throws InterruptedException {
+    public int await() throws InterruptedException { // how we indicate a certain thread has reached their barrier?
         // TODO Implement this function
+        // when a thread calls this method, it gets blocked until
+        // until the number of parties have also called await
+
+        // when all parties have called await all threads are released
+//        try{
+//            flag.acquire();
+//        }catch (InterruptedException e) {
+//
+//            // Print and display the line number where
+//            // exception occurred
+//            e.printStackTrace();
+//        }
+//        finally{
+//            while(flag.availablePermits() != 0){
+//                flag.release();
+//            }
+//
+//        }
+        flag.acquire();
+        activeParties++;
+        if(activeParties == parties){
+            flag.release(parties);
+        }
+        // ask about arrival index
+
         return -1;
     }
 
@@ -37,6 +70,11 @@ public class SemaphoreCyclicBarrier implements CyclicBarrier {
      */
     public void activate() throws InterruptedException {
         // TODO Implement this function
+        if(!this.barrierActive){
+            this.barrierActive = true;
+            // reset to its initial value?
+        }
+
     }
 
     /*
