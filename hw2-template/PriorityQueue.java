91e2caa81cb8f03e758effab99d6342358b813af
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-02-10 15:30:53 -0600

    add finished

--- hw2-template/PriorityQueue.java
+++ hw2-template/PriorityQueue.java
@@ -10,26 +10,37 @@ public class PriorityQueue {
 		private String name;
 		private Node next;
 		private ReentrantLock nodeLock;
-		private Condition notEmpty;
-		private Condition notFull;
 
 		public Node(String name, int priority){
 			this.name = name;
 			this.priority = priority;
 			this.nodeLock = new ReentrantLock();
-			this.notEmpty = nodeLock.newCondition();
-			this.notFull = nodeLock.newCondition();
 		}
 	}
 
 	private LinkedList<Node> linkedList;
 	private final int maxSize;
-	private Node head;
+	private ReentrantLock lock = new ReentrantLock();
+	Node head;
+	Node tail;
+	private int size;
+	private Condition notEmpty;
+	private Condition notFull;
+
 
 	public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
 		this.maxSize = maxSize;
 		this.linkedList = new LinkedList<>();
+		size = 0;
+		// dummy variables
+		head = new Node("head", -1);
+		tail = new Node("tail", 10);
+		head.next = tail;
+		tail.next = null;
+		lock.lock();
+		notEmpty = lock.newCondition();
+		notFull = lock.newCondition();
 	}
 
 	//Q: can i just lock one node and then the next?
@@ -45,80 +56,116 @@ public class PriorityQueue {
 		// but we can have a lock here that isn't specifically for a node
 		// rather a lock that would lock when a change is being made to the queue?
 
-		Node newNode = new Node(name, priority);
-		int index = 0;
-		Node prevNode = null;
-		Node currNode = head;
-		// do we add a lock?
+		// if not empty:
+		// firt lock the first two
+		// unlock head, lock tail
 
-		if(head == null){
-			head = newNode;
-			return index;
+		// lock node we're inserting
+		head.nodeLock.lock();
+		head.next.nodeLock.lock();
+		Node newNode = new Node(name, priority);
+		newNode.nodeLock.lock();
+
+		// case 1: queue is empty which is head points to tail and tail points to null
+		if((head.next == tail)){
+			head.next = newNode;
+			newNode.next = tail;
+			head.nodeLock.unlock();
+			tail.nodeLock.unlock();
+			newNode.nodeLock.unlock();
+			lock.unlock();
+			size++;
+			notEmpty.signal();
+			lock.lock();
+			return 0;
 		}
+
+
+		int index = 0;
+		//Node prevNode = null;
+		Node nextNode = head.next;
+		Node temp = null;
+		Node currNode = head;
 
 		// find the index of where the priority
 		// curr node no longer valid, duplicate, full
+		while(size == maxSize){
+			try {
+				// wait until node is removed from linked list when
+				// the linked list has reached its maximum size
+				notFull.await();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
 
-		// lock the current node
-		while(currNode != null){
-			// lock the current node to access its values
-			currNode.nodeLock.lock();
+		// lock the current node and next node
+		nextNode = currNode.next;
+		currNode.nodeLock.lock();
+		nextNode.nodeLock.lock();
+
+		while(currNode != tail){
+			// lock .next
+			// if first == second
+			// first.next = newNode
+			//node.next = tail
+			// unlock first and second
 			try{
-				if(currNode.priority < priority){
-					if(prevNode != null){
-						prevNode.nodeLock.unlock();
-					}
-					prevNode = currNode;
-					currNode = currNode.next;
+				if(currNode.name.equals(name)){  //if the object already exists in the list
+					currNode.nodeLock.unlock();
+					nextNode.nodeLock.unlock();
+					return -1;
+				}
+				if((currNode.priority < priority)  && (nextNode.priority > priority)){
+					//there is a space to insert, but check for duplicates
+					break;
 				}
 				else{
-					break;
+					currNode.nodeLock.unlock(); //release curr node
+					temp = nextNode.next; //get the one to hand over
+					currNode = nextNode; //update the current node to be next
+					nextNode = temp;
+					nextNode.nodeLock.lock(); //lock the "new" next node
 				}
 			} finally {
 				currNode.nodeLock.unlock();
 			}
 		}
-		// if it does equal null, then the list is empty
 
+		//this while loop actually inserts the node if no duplicate
+		Node left = nextNode;
+		Node right= nextNode.next;
+		right.nodeLock.lock();
 
-
-		/*
-		TA said to combine these
-		//		for(Node currNode : linkedList){
-//			if(currNode.name.equals(newNode.name)){
-//				return -1;
-//			}
-//			if(currNode.priority >= priority){
-//				break;
-//			}
-//			index++;
-//		}
-//
-//		for(int i = 0; i < index; i ++) {
-//			prevNode = currNode;
-//			currNode.nodeLock.lock();
-//			currNode = linkedList.get(i + 1);
-//		}
-//		if(prevNode != null){
-//			prevNode.nodeLock.unlock();
-//		}
-		 */
-
-		while(linkedList.size() == maxSize){
-			try {
-				// wait until node is removed from linked list when
-				// the linked list has reached its maximum size
-				currNode.notFull.await();
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
+		while(right != tail){
+			if(left.name.equals(name)){ //release everything and leave
+				currNode.nodeLock.unlock();
+				left.nodeLock.unlock();
+				right.nodeLock.unlock();
+				return -1;
+			}
+			else{
+				left.nodeLock.unlock();
+				temp = right.next;
+				left = right;
+				right = temp;
+				right.nodeLock.lock();
 			}
 		}
-		// get lock on current and the next one
-		linkedList.add(index, newNode);
+		right.nodeLock.unlock(); // unlock last node
 
-		// notEmpty Condition waits until a node has been added to the list
-		// to indicate that it is no longer empty
-		// this signals to any thread waiting in getFirst() that the list is not empty
+		Node second = currNode.next; // node right next to where the newNode will be inserted
+		second.nodeLock.lock(); //lock the two nodes btwn which new node will be inserted
+		currNode.next = newNode;
+		newNode.next = second;
+		currNode.nodeLock.unlock();
+		newNode.nodeLock.unlock();
+		second.nodeLock.unlock();
+		lock.unlock();
+		size++;
+		newNode.
+		lock.lock();
+
 
 		newNode.notEmpty.signal();
 		return index;
